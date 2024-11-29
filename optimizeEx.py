dc210a1b0ef81c72ac0670a6a85baa79d6d250fa
Nicola Gigante <88109014+NiGiga@users.noreply.github.com>
2024-11-29 21:13:36 +0100

    Update optimizeEx.py

--- optimizeEx.py
+++ optimizeEx.py
@@ -3,7 +3,27 @@ from z3 import Optimize, Int
 opt = Optimize()
 
 # Definisci le variabili
+x = Int('x')from z3 import Optimize, Int
+
+opt = Optimize()
+
+# Definisci le variabili
 x = Int('x')
+y = Int('y')
+
+# Aggiungi i vincoli
+opt.add(x > 0, y > 0)
+
+# Prima ottimizza x (minimizza il costo)
+opt.minimize(x)
+
+# Poi ottimizza y (minimizza la distanza)
+opt.minimize(y)
+
+# Risolvi
+if opt.check() == sat:
+    model = opt.model()
+    print("Optimal solution:", model)
 y = Int('y')
 
 # Aggiungi i vincoli
